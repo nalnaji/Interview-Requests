775b2ba4b2df0acce713d5ddf5b02fc2e06abf61
nalnaji <nalnaji@berkeley.edu>
2014-02-25 00:22:23 -0800

    Update FizzBuzz.java

---
+++ FizzBuzz.java
@@ -0,0 +1,21 @@
+public class FizzBuzz{
+
+     public static void main(String []args){
+        printFizzBuzz(37);
+     }
+     
+     public static void printFizzBuzz(int N)
+     {
+         for(int i = 1; i < N; i++)
+         {
+             if(i % 3 == 0)
+                System.out.println("fizz");
+             else if(i % 5 == 0)
+                System.out.println("buzz");
+             else
+                System.out.println(i+"");
+             
+         }
+         
+     }
+}
