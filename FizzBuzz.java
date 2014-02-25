58bd2884e5fe4593b92d1a75930c7fad61d9fbca
nalnaji <nalnaji@berkeley.edu>
2014-02-25 12:54:57 -0800

    Fizz buzz with case mod 3 and 5

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -6,9 +6,12 @@ public class FizzBuzz{
      
      public static void printFizzBuzz(int N)
      {
-         for(int i = 1; i < N; i++)
+         for(int i = 1; i <= N; i++)
          {
-             if(i % 3 == 0)
+             
+             if(i % 15 == 0)
+                 System.out.println("Fizz-Buzz");
+             else if(i % 3 == 0)
                 System.out.println("fizz");
              else if(i % 5 == 0)
                 System.out.println("buzz");
